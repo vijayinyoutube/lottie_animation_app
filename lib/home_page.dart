53e752297d6276bbecf51b29003aa4783ac6bc87
vijayinyoutube <vijaythevirtue@gmail.com>
2023-04-14 19:22:01 +0530

    refactor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,10 +7,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Lottie.network(
-          "https://assets1.lottiefiles.com/packages/lf20_obhph3sh.json",
-          width: 1000,
-          repeat: true),
+      body: Center(
+        child: Lottie.asset(
+          "assets/animations/building_animation.json",
+          repeat: true,
+        ),
+      ),
     );
   }
 }
